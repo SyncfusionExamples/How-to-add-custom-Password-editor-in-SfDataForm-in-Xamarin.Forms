b4e30435f33b28d04aa9982cf2a7cf47c9546c2f
Jeyasri <jeyasrim@syncfusion.com>
2018-10-31 12:49:15 +0530

    Sample committed

---
+++ CustomPasswordEditor/CustomPasswordEditor/CustomPasswordEditor/Model/Company.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CustomPasswordEditor
+{
+    public class Company : NotificationObject
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name should not be empty")]
+        public string Name { get; set; }
+       
+        public string Addresses { get; set; }
+        public string Password { get; set; }
+       
+        public Company()
+        {
+        }
+    }
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
